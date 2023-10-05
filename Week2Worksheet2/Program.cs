986104ba5397223632125010a222e67b458e3031
ISAAC ADEDARA <c3072624@hallam.shu.ac.uk>
2023-10-05 17:40:13 +0100

    Week 02 Worksheet 02 progress as at 17:39 on 5th October

---
+++ Week2Worksheet2/Program.cs
@@ -0,0 +1,53 @@
+﻿
+/*TASK 1 PART 1 
+ * 
+ * Console.Write("Enter a number to square: ");
+ * 
+int iNum = Convert.ToInt32(Console.ReadLine());
+int iSquared = funcSquare(iNum);
+
+Console.WriteLine($"{iNum} sqaured is {iSquared}");
+
+int funcSquare(int In)
+{
+    return In * In;
+}*/
+
+/*TASK 1 PART 2, 3
+
+//alls upon the ReadInt function and provides the input prompt for the user
+//int iAge = funcReadInt("Please enter your age:");
+
+//Creates the function funcReadInt and takes a question from the main method and prints it and in return it allows the user to input a number
+//int funcReadInt(string sQuestion)
+{
+    Console.WriteLine(sQuestion);
+    return Convert.ToInt32(Console.ReadLine());   
+}*/
+
+/*TASK PART 4
+int iUserNum = funcReadInt("Please enter a number: ");
+bool bOdd = funcIsOdd(iUserNum);
+
+if (bOdd == false)
+{
+    Console.WriteLine($"The number {iUserNum} is an even number");
+}
+
+if (bOdd  == true)
+{
+    Console.WriteLine($"The number {iUserNum} is an odd number");
+}
+bool funcIsOdd(int iUserNum)
+{
+    if (iUserNum % 2 == 0)
+    {
+        return false;
+    }
+    else 
+    {
+        return true;    
+    }
+}*/
+
+
